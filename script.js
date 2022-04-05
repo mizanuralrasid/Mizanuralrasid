d7edca21d280990fdddf940828c86e44aae4fb6c
MizanurAlrasid <88093931+mizanuralrasid@users.noreply.github.com>
2022-04-05 13:25:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -70,18 +70,5 @@ $(document).ready(function(){
             }
         }
     });
-    var dark = document.getElementById("dark");
-    dark.onclick = function(){
-    document.body.classList.toggle("dark-theme");
-    if(document.body.classList.contains("dark-theme")){
-        dark.src="images/sun.png";
-    }
-    else{
-        dark.src="images/moon.png";
-    }
-    }
-    AOS.init({
-       offset:280,
-       duration:1000
-    })
+
 });
